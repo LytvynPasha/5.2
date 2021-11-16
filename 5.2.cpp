ae997c5b8e365a2fdc556ca8a311e289e93684b2
Pasha <91208763+LytvynPasha@users.noreply.github.com>
2021-11-16 19:06:21 +0200

    Add files via upload

---
+++ 5.2.cpp
@@ -0,0 +1,56 @@
+﻿// Лабораторна робота No 5.2
+#include <iostream>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+double S(const double x, const double eps, int& n, double s);
+double A(const double x, const int n, double a);
+int main()
+{
+	double xp, xk, x, dx, eps, a = 0, R = 0, s = 0;
+	int n = 0;
+	cout << "xp = "; cin >> xp;
+	cout << "xk = "; cin >> xk;
+	cout << "dx = "; cin >> dx;
+	cout << "eps = "; cin >> eps;
+	cout << fixed;
+	cout << "-------------------------------------------------" << endl;
+	cout << "|" << setw(5) << "x" << " |"
+		<< setw(10) << "cos(x)" << " |"
+		<< setw(7) << "S" << " |"
+		<< setw(5) << "n" << " |"
+		<< endl;
+	cout << "-------------------------------------------------" << endl;
+	x = xp;
+	while (x <= xk)
+	{
+		s = S(x, eps, n, s);
+		cout << "|" << setw(7) << setprecision(2) << x << " |"
+			<< setw(10) << setprecision(5) << cos(x) << " |"
+			<< setw(10) << setprecision(5) << s << " |"
+			<< setw(5) << n << " |"
+			<< endl;
+
+		x += dx;
+	}
+	cout << "-------------------------------------------------" << endl;
+	return 0;
+}
+double S(const double x, const double eps, int& n, double s)
+{
+	n = 0; // вираз залежить від умови завдання варіанту
+	double a = ((pow(-1, n) * pow(x, 2 * n)) / (2 * n)); // вираз залежить від умови завдання варіанту
+	s = a;
+	do {
+		n++;
+			a = A(x, n, a);
+		s += a;
+	} while (abs(a) >= eps);
+	return s;
+}
+double A(const double x, const int n, double a)
+{
+	double R = -1 * (x * x * tgamma(2 * n - 2)) / tgamma(2 * n); // вираз залежить від умови завдання варіанту
+	a *= R;
+	return a;
+}
